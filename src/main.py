25fc99f49d93ce7b6066a04f33968420ff4b1372
Miguel Amato <miamato@ucm.es>
2025-03-20 15:32:12 +0100

    chore: Add saving in a json file the result

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
 import os
 import json
+from dotenv import load_dotenv
 from questionnaire.agent import QuestionnarieAgent
 from questionnaire.schemas import Questionnaire
+
+load_dotenv()
+
+DATA_PATH = os.getenv("DATA_PATH")
+
 
 def main():
     os.system("cls" if os.name == "nt" else "clear")
@@ -11,6 +17,10 @@ def main():
     response = questionnarie.call_llm(text=text, response_format=Questionnaire)
     print("\n\n")
     print(json.dumps(response.dict(), indent=4, ensure_ascii=False))
+    path = os.path.join(DATA_PATH, "result.json")
+    with open(path, "w") as file:
+        json.dump(response.model_dump(), file, indent=4)
+    print("JSON succesfully saved!")
 
 
 if __name__ == "__main__":
